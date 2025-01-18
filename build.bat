e487391f6a91ba7570216a95a4064c5b87180f8a
edw0rd21 <www.srijitmondal210@gmail.com>
2025-01-18 06:23:19 +0530

    added batch script

--- build.bat
+++ build.bat
@@ -1,9 +1,27 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set "ASSET_PATH=..\AssetFile"
-set "DEST_DIR=bin\Release"
+set BUILD_DIR=build
+set BUILD_TYPE=Release
+set "ASSET_PATH=AssetFile"
+set "DEST_DIR=build\bin\Release"
 
-cmake --build . --config Release
+if not exist "%BUILD_DIR%" (
+    echo Creating build directory: %BUILD_DIR%
+    mkdir "%BUILD_DIR%"
+)
 
-xcopy "%ASSET_PATH%" "%DEST_DIR%" /E /I /Y
+cd "%BUILD_DIR%"
+
+echo Configuring the project...
+cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+
+echo Building the project...
+cmake --build . --config %BUILD_TYPE%
+
+cd ..
+
+echo Copying assets to the destination directory: %DEST_DIR%
+xcopy "%ASSET_PATH%" "%DEST_DIR%\AssetFile" /E /I /Y
+
+echo Build complete!
